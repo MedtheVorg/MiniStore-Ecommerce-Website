730e0ac3bb7dc991a9de0fd8ad97053556d4ff60
MedtheVorg <mohamedlem216@gmail.com>
2023-10-05 00:08:47 +0100

    vercel config

--- server/routers/user/user.controller.ts
+++ server/routers/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { User } from './user.model.ts';
+import { User } from './user.model.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
